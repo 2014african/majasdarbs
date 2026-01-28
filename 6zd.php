96086553a248c307185d5c68a9c6c928231922f4
Emils <emils@gmail.com>
2026-01-28 10:35:31 +0200

    iesakts 7 uzd

--- 6zd.php
+++ 6zd.php
@@ -1,9 +1,12 @@
 <?php
+$result = "Color: ";
 $colors = ['red', 'blue', 'green', 'yellow'];
 foreach($colors as $color)
     {
-        $result = $result . $color;
-        echo $color . ", ";
+        $result = $result . $color . ", ";
+        // echo $color . ", ";
     }
+echo $result;
 
+if ($i == count($fruits))
 ?>
--- darbs.php
+++ darbs.php
@@ -17,19 +17,36 @@
     // for($i = 0; $i < count($students); $i++) {
         //     echo $students[$i];
         // }
-        $students = array(
-           array("Vārds:"=>"Džons", "Vecums:"=>20, "Atzimes:"=>[90,85,88]),
-            array("Vārds:"=>"Dzena", "Vecums:"=>22, "Atzimes:"=>[92,80,84]),
-            array("Vārds:"=>"Džo", "Vecums:"=>21, "Atzimes:"=>[78,85,90]),
-        );
+//         $students = array(
+//            array("Vārds:"=>"Džons", "Vecums:"=>20, "Atzimes:"=>[90,85,88]),
+//             array("Vārds:"=>"Dzena", "Vecums:"=>22, "Atzimes:"=>[92,80,84]),
+//             array("Vārds:"=>"Džo", "Vecums:"=>21, "Atzimes:"=>[78,85,90]),
+//         );
 
-        $sum = 0;
-foreach($students as $person) 
+//         $sum = 0;
+// foreach($students as $person) 
+//     {
+//         foreach($person["Atzimes:"] as $grade)
+
+//             $sum = $sum + $grade;
+//     }
+//     $avg = $sum / 6;
+//     echo $avg;
+// // echo $students[1]["Vecums:"][0];
+
+
+
+$people = array(
+    ["Vārds" => "Jānis", "Vecums" => 20, "E-pasts" => "janis@janis.lv", "Valodas" => "PHP, Python, Java"],
+    ["Vārds" => "Antra", "Vecums" => 34, "E-pasts" => "antra@antra.com", "Valodas" => "HTML, Javascript, SQL"],
+);
+
+for ($i = 0; $i < count($people); $i++)
     {
-        foreach($person["Atzimes:"] as $grade)
+        if ($i == count($people) - 1)
+        {
+            
+        }
+        echo $people[$i];
 
-            $sum = $sum + $grade;
     }
-    $avg = $sum / 6;
-    echo $avg;
-// echo $students[1]["Vecums:"][0];
